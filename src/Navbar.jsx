53cf772a59e2b5b8ed2f4bfc225c9bf8ce0a5c35
Dilsinho7 <dilsinhop399@gmail.com>
2025-05-14 17:53:39 -0300

    alteração na NavBar e TopBar por Adilson

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,3 +1,11 @@
+import iconeDashboard from "./assets/img/iconeDashboard.png";
+import iconeOKR from "./assets/img/iconeOKR.png";
+import iconeRelatorio from "./assets/img/iconeRelatorio.png";
+import iconeMetas from "./assets/img/iconeMetas.png";
+import iconeConfig from "./assets/img/iconeConfig.png";
+import iconeAjuda from "./assets/img/iconeAjuda.png";
+import iconeSobre from "./assets/img/iconeSobre.png";
+import iconeSair from "./assets/img/iconeSair.png";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
@@ -14,45 +22,46 @@ function Navbar() {
   return (
     <div className={`sidebar ${isExpanded ? "expanded" : "collapsed"}`}>
       <div className="top-bar">
-        <button onClick={toggleSidebar}>☰</button>
+        <button className="NavButton" onClick={toggleSidebar}>☰</button>
       </div>
 
       <div className="menu">
         <div className="menu-item">
-          📈 {isExpanded && <span> <Link to={"Home"} >Dashboard Principal</Link> </span>}
+          <img className="StyleIcones" src={iconeDashboard}/> {isExpanded && <span className="NavText"> <Link to={"Home"} >Dashboard <br/> Principal</Link> </span>}
         </div>
-
-        <div onClick={() => toggleSubMenu("okrs")} className="menu-item">
-          📊 {isExpanded && <span>OKRs</span>}
-        </div>
-        {activeMenu === "okrs" && isExpanded && (
-          <div className="submenu">
-            <div className="submenu-item"> <Link to={"VisuOKR"}>Visualizar OKRs</Link> </div>
-            <div className="submenu-item"> <Link to={"CadastrarOKR"}>Cadastrar OKR</Link> </div>
+        <div className="with-submenu">  
+          <div onClick={() => toggleSubMenu("okrs")} className="menu-item">
+            <img className="StyleIcones" src={iconeOKR}/> {isExpanded && <span className="NavText" >OKRs</span>}
           </div>
-        )}
-
-        <div className="menu-item">
-          📋 {isExpanded && <span>Relatórios</span>}
+          {activeMenu === "okrs" && isExpanded && (
+            <div className="submenu">
+              <div className="submenu-item"> <Link to={"VisuOKR"}>Visualizar OKRs</Link> </div>
+              <div className="submenu-item"> <Link to={"CadastrarOKR"}>Cadastrar OKR</Link> </div>
+            </div>
+          )}
         </div>
 
         <div className="menu-item">
-          ⭐ {isExpanded && <span> <Link to={"CriarMetas"}>Criar Metas</Link> </span>}
+          <img className="StyleIcones" src={iconeRelatorio}/> {isExpanded && <span className="NavText">Relatórios</span>}
         </div>
 
-        <hr style={{ borderColor: 'rgba(255,255,255,0.2)', margin: '1rem 0' }} />
+        <div className="menu-item">
+          <img className="StyleIcones" src={iconeMetas}/> {isExpanded && <span className="NavText" > <Link to={"CriarMetas"}>Criar Metas</Link> </span>}
+        </div>
+
+        <hr style={{ borderColor: 'rgba(255,255,255,0.2)', margin: '1rem 0px 8.5rem 0px ' }} />
 
         <div className="menu-item">
-          ⚙️ {isExpanded && <span>Configurações</span>}
+          <img className="StyleIcones" src={iconeConfig}/> {isExpanded && <span className="NavText" >Configurações</span>}
         </div>
         <div className="menu-item">
-          ❓ {isExpanded && <span>Ajuda e Suporte</span>}
+          <img className="StyleIcones" src={iconeAjuda}/> {isExpanded && <span className="NavText" >Ajuda e Suporte</span>}
         </div>
         <div className="menu-item">
-          📨 {isExpanded && <span>Sobre</span>}
+          <img className="StyleIcones" src={iconeSobre}/> {isExpanded && <span className="NavText" >Sobre</span>}
         </div>
         <div className="menu-item">
-          🚪 {isExpanded && <span>Sair</span>}
+          <img className="StyleIcones" src={iconeSair}/> {isExpanded && <span className="NavText" >Sair</span>}
         </div>
       </div>
     </div>
--- src/Topbar.jsx
+++ src/Topbar.jsx
@@ -1,17 +1,23 @@
-
+import imgBanco from './assets/img/img-branco.png';
+import "./index.css";
+import iconeRobo from './assets/img/iconeRobo.png';
 
 
 function Topbar() {
   return (
     <div style={{
-      backgroundColor: "#ffeb3b",
-      padding: "1.1rem",
+      backgroundColor: "#FCFB30",
+      padding: "0.78rem 2rem 0.78rem 0",
+      height: 53,
       display: "flex",
       justifyContent: "space-between",
       alignItems: "center"
     }}>
-      <div>Menu</div>
-      <div>ADM</div>
+      <div><img className='Logobanco' src={imgBanco}/></div>
+      <div style={{width: '90px' ,display:'flex', alignItems:'center', justifyContent: 'space-between'}} >
+        <div> <img style={{width: '20px'}} src={iconeRobo} /> </div>
+        <div style={{fontFamily: 'Montserrat, sans-serif', color: '#465EFF', fontWeight: 600, fontSize: '14px'}} >ADM</div>
+      </div>
     </div>
   );
 }
